439bf1cf1aea3a7bb56f2cf1b13e401652991c6e
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2025-10-14 17:44:41 +0200

    Doesn't brick your chip now
    Which is better.
    LED pin moved to 5.

--- src/led/main.cpp
+++ src/led/main.cpp
@@ -1,23 +1,23 @@
 #include "ch32fun.h"
 
-#define LED_PIN PD4
-#define LED_PORT GPIOD
-#define LED_PIN_MASK (1 << 4)
+#define LED_PIN PC1
+#define LED_PORT GPIOC
+#define LED_PIN_MASK (1 << 1)
 #define LED_CTL_REG CFGLR
-#define LED_CTL_SHIFT 16
-#define LED_MODE (1 << LED_CTL_SHIFT)
+#define LED_CTL_SHIFT 4
+#define LED_MODE (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)
 #define LED_CNF 0
 
 int main(void)
 {
-    RCC->APB2PCENR |= RCC_IOPDEN;
+    RCC->APB2PCENR |= RCC_IOPCEN;
     LED_PORT->LED_CTL_REG &= ~(0xf << LED_CTL_SHIFT);
-    LED_PORT->LED_CTL_REG |= LED_MODE | LED_CNF;
+    LED_PORT->LED_CTL_REG |= (LED_MODE << LED_CTL_SHIFT);
     while(1)
     {
         LED_PORT->BSHR = LED_PIN_MASK;
-        Delay_Ms(200);
+        Delay_Ms(100);
         LED_PORT->BSHR = (LED_PIN_MASK << 16);
-        Delay_Ms(200);
+        Delay_Ms(100);
     }
 }
